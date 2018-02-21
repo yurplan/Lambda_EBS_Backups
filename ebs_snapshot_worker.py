2e88aa913a08c501b1638be02b52df9f12f11ed3
TacMechMonkey <34081660+TacMechMonkey@users.noreply.github.com>
2018-02-21 13:23:37 +1000

    Update ebs_snapshot_worker.py

--- ebs_snapshot_worker.py
+++ ebs_snapshot_worker.py
@@ -3,138 +3,134 @@
 # http://blog.powerupcloud.com/2016/02/15/automate-ebs-snapshots-using-lambda-function/
 # Rewritten to be configured on individual Volumes, not Instances.
 # https://github.com/Brayyy/Lambda-EBS-Snapshot-Manager
+# Updated for Python 3.6, added local time to 4/day, added requirement for EBS volume to be 'in-use'
+# https://github.com/TacMechMonkey/Lambda_EBS_Backups-Python_3-6
 
 import boto3
 import datetime
 import time
 import os
 
-# Manually configure EC2 region, account IDs, timezone
-ec = boto3.client('ec2', region_name='us-east-1')
-aws_account_ids = ['020662911542']
-default_days_of_renention = 7
-os.environ['TZ'] = 'US/Eastern'
+RETENTION_DEFAULT = 90
+TIME_ZONE = 'Australia/Brisbane'
+AWS_REGION = 'ap-southeast-2'
+BACKUP_KEY = 'Backup'
+RETENTION_KEY = 'Retention'
 
-# Nothing to configure below this line
+if 'RETENTION_DEFAULT' in os.environ:
+    RETENTION_DEFAULT = int(os.environ['RETENTION_DEFAULT'])
+if 'TIME_ZONE' in os.environ:
+    TIME_ZONE = os.environ['TIME_ZONE']
+if 'AWS_REGION' in os.environ:
+    AWS_REGION = os.environ['AWS_REGION']
+if 'BACKUP_KEY' in os.environ:
+    BACKUP_KEY = os.environ['BACKUP_KEY']
+if 'RETENTION_KEY' in os.environ:
+    RETENTION_KEY = os.environ['RETENTION_KEY']
+
+EC2_CLIENT = boto3.client('ec2', region_name=AWS_REGION)
+os.environ['TZ'] = TIME_ZONE
 
 
-def create_new_backups():
+def create_snapshot():
 
-    # Current hour
     current_hour = int(datetime.datetime.now().strftime('%H'))
 
-    # Find volumes tagged with key "Backup"
-    volumes = ec.describe_volumes(
+    volumes = EC2_CLIENT.describe_volumes(
         Filters=[
-            {'Name': 'tag-key', 'Values': ['Backup']},
+            {'Name': 'tag-key', 'Values': [BACKUP_KEY]},
+            {'Name': 'status', 'Values': ['in-use']},
         ]
     ).get(
-       'Volumes', []
+        'Volumes', []
     )
 
-    print 'Number of volumes with backup tag: %d' % len(volumes)
+    print ("Number of volumes with backup tag: %d" % len(volumes))
 
     for volume in volumes:
         vol_id = volume['VolumeId']
-        vol_retention = default_days_of_renention
-        snap_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        vol_retention = RETENTION_DEFAULT
+        snap_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
         snap_desc = vol_id
-
-        # Loop over all tags and grab out relevant keys
+        
         for name in volume['Tags']:
             tag_key = name['Key']
             tag_val = name['Value']
-            # print '...Found EBS volume key %s : %s ' % ( tag_key, tag_val );
 
-            # Below the code is create Snapshot name as instance Name
             if tag_key == 'Name':
                 snap_desc = vol_id + ' (' + tag_val + ')'
 
-            if tag_key == 'Retention' and tag_val.isdigit():
+            if tag_key == RETENTION_KEY and tag_val.isdigit():
                 vol_retention = int(tag_val)
 
-            if tag_key == 'Backup':
+            if tag_key == BACKUP_KEY:
                 backup_mod = False
-                if tag_val == '' or tag_val == 'false':
+                if tag_val == '' or tag_val == 'No' or tag_val == 'false' :
                     backup_mod = False
-                elif tag_val == 'true':
+                elif tag_val == 'Weekly':
+                    backup_mod = 168
+                elif tag_val == 'Daily':
                     backup_mod = 24
-                elif tag_val == 'daily':
-                    backup_mod = 24
-                elif tag_val == '1/day':
-                    backup_mod = 24
-                elif tag_val == '2/day':
-                    backup_mod = 12
                 elif tag_val == '4/day':
                     backup_mod = 6
-                elif tag_val == '6/day':
-                    backup_mod = 4
-                elif tag_val == '8/day':
-                    backup_mod = 3
-                elif tag_val == '12/day':
-                    backup_mod = 2
-                elif tag_val == '24/day':
-                    backup_mod = 1
-                elif tag_val == 'hourly':
+                elif tag_val == 'Hourly':
                     backup_mod = 1
                 else:
-                    print '%s unknown backup schedule %s' % (vol_id, tag_val)
+                    print ("%s unknown backup schedule %s" % (vol_id, tag_val))
                     continue
 
         snap_name = 'Backup of ' + snap_desc
         snap_desc = 'Lambda backup ' + snap_date + ' of ' + snap_desc
         delete_ts = '%.0f' % ((vol_retention * 86400) + time.time())
 
-        # Only backup if scheduled to do so
-        if backup_mod is False or current_hour % backup_mod != 0:
-            print '%s is not scheduled this hour' % vol_id
+        if backup_mod is False or (current_hour + 10) % backup_mod != 0:
+            print ("%s is not scheduled this hour" % vol_id)
             continue
         else:
-            print '%s is scheduled this hour' % vol_id
+            print ("%s is scheduled this hour" % vol_id)
 
-        # Create snapshot
-        snap = ec.create_snapshot(
-          VolumeId=vol_id,
-          Description=snap_desc,
+        snap = EC2_CLIENT.create_snapshot(
+            VolumeId=vol_id,
+            Description=snap_desc,
         )
 
-        print 'snap %s' % snap
+        print ("%s created" % snap['SnapshotId'])
 
-        ec.create_tags(
-          Resources=[snap['SnapshotId']],
-          Tags=[
-            {'Key': 'Name', 'Value': snap_name},
-            {'Key': 'DeleteAfter', 'Value': str(delete_ts)}
-          ]
+        EC2_CLIENT.create_tags(
+            Resources=[snap['SnapshotId']],
+            Tags=[
+                {'Key': 'Name', 'Value': snap_name},
+                {'Key': 'Delete After', 'Value': str(delete_ts)}
+            ]
         )
 
 
-def destroy_old_backups():
+def delete_old_backups(aws_account_ids):
     filters = [
-        {'Name': 'tag-key', 'Values': ['DeleteAfter']}
+        {'Name': 'tag-key', 'Values': ['Delete After']}
     ]
-    snapshot_response = ec.describe_snapshots(
-      OwnerIds=aws_account_ids,
-      Filters=filters
+    snapshot_response = EC2_CLIENT.describe_snapshots(
+        OwnerIds=aws_account_ids,
+        Filters=filters
     )
-    # print snapshot_response
 
     for snap in snapshot_response['Snapshots']:
-        # Loop over all tags and grab out relevant keys
         for name in snap['Tags']:
             tag_key = name['Key']
             tag_val = name['Value']
-            # print '...Found EBS volume key %s : %s ' % ( tag_key, tag_val );
 
-            if tag_key == 'DeleteAfter':
+            if tag_key == 'Delete After':
                 if int(tag_val) < time.time():
-                    print '%s is being deleted' % snap['SnapshotId']
-                    ec.delete_snapshot(SnapshotId=snap['SnapshotId'])
+                    print ("%s is being deleted" % snap['SnapshotId'])
+                    EC2_CLIENT.delete_snapshot(SnapshotId=snap['SnapshotId'])
                 else:
-                    print '%s is safe.' % snap['SnapshotId']
+                    print ("%s is safe." % snap['SnapshotId'])
 
 
 def lambda_handler(event, context):
-    create_new_backups()
-    destroy_old_backups()
-    return 'successful'
+    aws_account_ids = [context.invoked_function_arn.split(":")[4]]
+
+    create_snapshot()
+    delete_old_backups(aws_account_ids)
+
+    return "Successful"
